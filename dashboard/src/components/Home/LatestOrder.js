a240d510bf589afed1bb7f1e1807a2f4e760bcd2
Miled-abdelhamid <miledabdelhamid@gmail.com>
2022-03-08 23:09:25 +0100

    Project

---
+++ dashboard/src/components/Home/LatestOrder.js
@@ -0,0 +1,29 @@
+import moment from "moment";
+import React from "react";
+import { Link } from "react-router-dom";
+import Message from "../LoadingError/Error";
+import Loading from "../LoadingError/Loading";
+
+const LatestOrder = (props) => {
+  const { loading, error, orders } = props;
+  return (
+    <div className="card-body">
+      <h4 className="card-title">New orders</h4>
+      {loading ? (
+        <Loading />
+      ) : error ? (
+        <Message variant="alert-danger">{error}</Message>
+      ) : (
+        <div className="table-responsive">
+          <table className="table">
+            <tbody>
+             
+            </tbody>
+          </table>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default LatestOrder;
